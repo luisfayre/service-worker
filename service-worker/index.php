09ba53996248c45fb9ee082b3b2fbee8198658a5
Luisfayre <luisfayre@hotmail.com>
2019-08-18 09:55:45 -0600

    Add files via upload

---
+++ service-worker/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Service Worker</title>
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
+
+    </head>
+    <body>
+
+        <div class="container">
+            <h1>Hola</h1>
+
+            <br>
+            <br>
+            
+            <img class="img-fluid"src="img/prueba.png">
+            <br>
+            <br>
+            <p>Este es un texto de prueba</p>
+
+        </div>
+
+
+        <script src="js/sw.js"/>
+    </body>
+</html>
